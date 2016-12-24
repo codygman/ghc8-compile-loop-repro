647ba11493d7c38865ace12995575eb5689478a0
Cody Goodman <codygman.consulting@gmail.com>
2016-12-24 17:18:28 -0600

    add code example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE ConstraintKinds, DataKinds, FlexibleContexts, GADTs,
+             OverloadedStrings, PatternSynonyms, QuasiQuotes,
+             ScopedTypeVariables, TemplateHaskell, TypeOperators,
+             ViewPatterns #-}
 module Main where
+
+import Frames
+import Frames.CSV
+import qualified Pipes.Prelude as P
+import Pipes hiding (Proxy)
+
+-- reads column names from a 176 column csv with format: Text,Int,Bool,Double,Bool,Int repeated 29.333333333333332 times
+tableTypes' rowGen "sample.csv"
+
+rowStream :: Producer Row IO ()
+rowStream = readTableOpt rowParser "sample.csv"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  P.length rowStream >>= print
+  -- runEffect $ rowStream >-> P.print
